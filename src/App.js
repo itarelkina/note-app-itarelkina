b0ab8969baf90b3c5bb4e421645eca46124c185d
Irina Tarelkina <itarelkina@yahoo.ca>
2019-06-05 23:21:17 +0000

    add a new tag disappears after being clicked

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
     this.state = {
       showNote: false,
       notes: [],
-      note: {}
+      note: {},
+      newTag: false
     };
   }
 
@@ -55,9 +56,14 @@ class App extends Component {
     .then((res) => this.setState({ notes: newNotesState }))
     .catch((err) => console.log(err.response.data) );
   }
+
+  showTagForm = () => {
+    this.setState({ newTag: true });
+  }
   
   render() {
-    const { showNote, notes, note } = this.state;
+    const { showNote, notes, note, newTag } = this.state;
+
 
     return (
       <div className="App">
@@ -66,6 +72,8 @@ class App extends Component {
         <Note 
         note={note}
         submitNote={this.submitNote}
+        showTagForm={this.showTagForm}
+        newTag={newTag}
         />
         :
         <List
