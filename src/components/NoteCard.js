e4c5725989b79d1f1e98fbc117b6671b21d6bf67
Irina Tarelkina <itarelkina@yahoo.ca>
2019-06-05 19:04:26 +0000

    added state component

---
+++ src/components/NoteCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NoteCard extends React.Component {
+  render() {
+    return (
+      <div className="note-card-container">
+        NoteCard Component
+      </div>
+    );
+  }
+}
+
+export default NoteCard;
