53ccfae0472ef9e3fe5c6ca167404c0ef55feb18
Irina Tarelkina <itarelkina@yahoo.ca>
2019-06-05 22:59:59 +0000

    The performSubmissionRequest method

--- src/App.js
+++ src/App.js
@@ -34,9 +34,20 @@ class App extends Component {
     .catch((err) => console.log(err.response.data) );
   }
 
-  submitNote = (data) => {
-    console.log(data);
+  performSubmissionRequest = (data, id) => {
+    if (id) {
+      return axios.patch(urlFor(`notes/${id}`), data);
+    } else {
+      return axios.post(urlFor('notes'), data);
+    }
   }
+  
+  submitNote = (data, id) => {
+    this.performSubmissionRequest(data, id)
+    .then((res) => this.setState({ showNote: false }) )
+    .catch((err) => console.log(err.response.data) );
+  }
+  
 
   render() {
     const { showNote, notes, note } = this.state;
